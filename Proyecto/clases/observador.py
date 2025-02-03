1df5790438146fb2f8bd1016ccb19204d0096a62
Shorsh7 <166432820+Shorsh7@users.noreply.github.com>
2025-02-02 23:07:16 -0300

    classes deleted, minor code tweeks

--- Proyecto/clases/observador.py
+++ Proyecto/clases/observador.py
@@ -46,38 +46,8 @@ class ObservadorConcretoAgregar (Observador): #tomar este pbjeto de ejemplo para
         self.hora_estado = self.observado_agregar.get_hora_estado()  
         print(f"Estado = {self.estado} | Hora = {self.hora_estado}") 
 
-class ObservadorConcretoModificar (Observador):
-    def __init__(self, object):
-        self.observado_modificar = object
-        self.observado_modificar.agregar (self)
-
-    def update (self, *args):
-        print ("Actualización dentro de Observador ObservadorConcretoModificar", args)
-        self.estado = self.observado_modificar.get_estado ()
-        self.hora_estado = self.observado_modificar.get_hora_estado()  
-        print(f"Estado = {self.estado} | Hora = {self.hora_estado}")
-
-class ObservadorConcretoEliminar (Observador):
-    def __init__(self, object):
-        self.observado_eliminar = object
-        self.observado_eliminar.agregar (self)
-
-    def update (self):
-        print ("Actualización dentro de Observador ObservadorConcretoEliminar")
-        self.estado = self.observado_eliminar.get_estado ()
-        print ("Estado = ", self.estado)
-
 
 tema1 = PokemonCollection ()
 
 observador_a = ObservadorConcretoAgregar (tema1)
 tema1.set_estado("Se agregó un pokemon con éxito", notificar=False)
-
-# tema2 = PokemonCollection ()
-
-# observador_b = ObservadorConcretoModificar (tema2)
-# tema2.set_estado ("Se modificó un pokemon con éxito")
-
-# tema3 = PokemonCollection ()
-# observador_c = ObservadorConcretoEliminar (tema3)
-# tema3.set_estado ("Se eliminó un pokemon con éxito")
